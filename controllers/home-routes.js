3e4917440396fbb7659bc43f4218aa7067b4ae2b
eljayman <lelandoj@gmail.com>
2022-12-31 19:40:35 -0700

    added session storage, css styling, date format helper

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const { Blog, User } = require('../models');
 
+//homepage view all blog entries
 router.get('/', async (req, res) => {
   try {
+    //get all blogs and user's name
     const blogData = await Blog.findAll({
       include: [
         {
@@ -15,10 +17,9 @@ router.get('/', async (req, res) => {
     // Serialize data so the template can read it
     const blogs = blogData.map((blog) => blog.get({ plain: true }));
 
-    // Pass serialized data and session flag into template
+    // Pass serialized data into template
     res.render('home', {
       blogs,
-      // logged_in: req.session.logged_in,
     });
   } catch (err) {
     res.status(500).json(err);
--- server.js
+++ server.js
@@ -1,27 +1,32 @@
 const express = require('express');
-// const session = require('express-session');
+const session = require('express-session');
 const routes = require('./controllers');
 const path = require('path');
 const sequelize = require('./config/connection');
-// const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const exphbs = require('express-handlebars');
-// const helpers = require('./utils/helpers');
+const helpers = require('./utils/helpers');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// const sess = {
-//   secret: 'Super secret secret',
-//   cookie: {},
-//   resave: false,
-//   saveUninitialized: true,
-//   store: new SequelizeStore({
-//     db: sequelize,
-//   }),
-// };
-const hbs = exphbs.create();
+const ONE_DAY_IN_MILLISECONDS = 24 * 60 * 60 * 1000;
+
+const sess = {
+  secret: process.env.SECRET,
+  cookie: {
+    maxAge: ONE_DAY_IN_MILLISECONDS,
+  },
+  resave: false,
+  saveUninitialized: true,
+  name: 'tblg.sid',
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+app.use(session(sess));
+const hbs = exphbs.create({ helpers });
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
-// app.use(session(sess));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
